9eba76d2b2c3bc9d0ee7a317dee952d60b957ad9
Cristóbal Ganter <cganterh@synopsys.com>
2025-05-27 20:45:49 +0200

    Disable spell checking in the terminal

--- init.lua
+++ init.lua
@@ -266,6 +266,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Disable spell checking in the terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Disable spell checking in the terminal',
+  pattern = '*',
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
